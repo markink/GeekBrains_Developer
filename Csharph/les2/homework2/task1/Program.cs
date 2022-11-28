0ad8902296ab4c32acb94b63d702f20614fc8fe9
Markin Konstantin <markin_konstantin@vk.com>
2022-11-28 17:00:57 +0500

    Программа, которая показывает вторую цифру введённого трёхзначного числа.

---
+++ Csharph/les2/homework2/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: Напишите программу, которая принимает 
+// на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа. Не использовать строки
+
+
+int Promt(string message)
+{
+    System.Console.Write($"{message}");
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+int input1 = Promt("Введите трёхзначное число ");
+
+int ostatok = input1/10%10;
+
+if (input1 >= 100 && input1 < 1000)
+{
+    Console.WriteLine(ostatok);
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное");
+}
