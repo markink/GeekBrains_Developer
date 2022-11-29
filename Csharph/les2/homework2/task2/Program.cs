a6d1da325eab92e2321653d80406f9e6943d8699
Markin Konstantin <markin_konstantin@vk.com>
2022-11-29 15:37:33 +0500

    Создали программу, которая удаляет строки для расчёта.

---
+++ Csharph/les2/homework2/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// : Напишите программу, 
+// которая выводит случайное трёхзначное число и
+//  удаляет вторую цифру этого числа. 
+//  Не использовать строки для расчета
+
+int randomValue = new Random().Next(100, 1000);
+
+Console.WriteLine(randomValue);
+
+int sotni = randomValue/100;
+int desyatki = randomValue/10%10;
+int edinici = randomValue%10;
+
+int OutputValue = sotni*10+edinici;
+
+Console.WriteLine(sotni+" "+desyatki+" "+edinici);
+
+Console.WriteLine(OutputValue);
