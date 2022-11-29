6b601950ee6cbdb198a5a292141d97c89c964766
Markin Konstantin <markin_konstantin@vk.com>
2022-11-29 16:36:34 +0500

    Создали программу, которая показывает выходной день или нет.

---
+++ Csharph/les2/homework2/task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу,
+//  которая принимает на вход цифру, 
+//  обозначающую день недели, 
+//  и проверяет, является ли этот день выходным. 
+//  Не использовать строки
+
+int Promt(string message)
+{
+    System.Console.Write($"{message}");
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+int InputValue = Promt("Введи цифру дня недели: ");
+
+if (InputValue >= 1 && InputValue <= 5) {
+    Console.Write("Нет");
+} else {
+    Console.Write("Да");
+}
