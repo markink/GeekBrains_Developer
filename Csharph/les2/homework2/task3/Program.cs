ff7d6cb8992594557e2379c8a867d22f2f280de0
Markin Konstantin <markin_konstantin@vk.com>
2022-11-29 17:15:34 +0500

    Добавили программу, которая выводит 3 цифры числа.

---
+++ Csharph/les2/homework2/task3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, 
+// которая выводит третью цифру заданного числа или сообщает
+// что третьей цифры нет. . Не использовать строки
+
+int Promt(string message)
+{
+    System.Console.Write($"{message}");
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+int InputValue = Promt("Введите число: ");
+
+int arrayIndex = 1;
+
+int Condition = InputValue;
+int InputValue1 = InputValue;
+
+while (Condition >= 10)
+{
+    Condition = Condition / 10;
+    arrayIndex = arrayIndex + 1;
+}
+
+int [] array = new int [arrayIndex];
+
+for (int i=0; i<array.Length; i++){
+    array [i] = InputValue1%10;
+    InputValue1 = InputValue1/10;
+}
+
+Array.Reverse(array);
+
+if (InputValue < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine("Третьей цифрa: " + array[2]);
+}
