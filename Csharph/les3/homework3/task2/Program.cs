5ef170bb0deb30f0f4461a2524d0d642d624f974
Markin Konstantin <markin_konstantin@vk.com>
2022-11-30 13:34:18 +0500

    Добавили программу которая находит расстояния между точками в 3D пространстве

---
+++ Csharph/les3/homework3/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает 
+// на вход координаты двух точек и находит
+//  расстояние между ними в 3D пространстве.
+
+int Promt(string message)
+{
+    System.Console.Write($"{message}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int x1 = Promt("Введите x1");
+int x2 = Promt("Введите x2");
+int y1 = Promt("Введите y1");
+int y2 = Promt("Введите y2");
+int z1 = Promt("Введите z1");
+int z2 = Promt("Введите z2");
+
+int deltax = x1-x2;
+int deltay = y1-y2;
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+System.Console.WriteLine(distance);
