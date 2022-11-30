f301192d4220350d9a368c7d468c81c1becbb293
Markin Konstantin <markin_konstantin@vk.com>
2022-11-30 13:44:16 +0500

    Создали программу, которая выводит таблицу кубов от 1 до введённого числа

---
+++ Csharph/les3/homework3/task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает 
+// на вход число (N) и выдаёт таблицу 
+// кубов чисел от 1 до N.
+
+int Promt(string message)
+{
+    System.Console.Write($"{message}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int value1 = Promt("Введите число: ");
+
+if (value1 > 0)
+{
+    for (int i = 1; i <= value1; i++)
+    {
+        Console.Write(i * i * i + " ");
+    }
+}
+else
+{
+    for (int i = 1; i >= value1; i--)
+    {
+        Console.Write(i * i * i + " ");
+    }
+}
+
