230ba062249259018a78ad9372bd64737e034f72
Markin Konstantin <markin_konstantin@vk.com>
2022-12-03 12:57:24 +0500

    Вычисление факториала

---
+++ Csharph/les4/les4/task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на
+//  вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+
+
+int Promt(String message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int value = Promt("Введите число: ");
+
+int Vichislenie (int value) {
+    int value2 = 1;
+    for (int i=1; i<=value; i++){
+        value2 = value2 * i;
+    
+    }
+    return value2;
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {value} будет равно {Vichislenie(value)}");
