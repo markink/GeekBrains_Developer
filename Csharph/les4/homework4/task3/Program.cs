a8b8abf4da35acb333d17277ed16b6e219b767cc
Markin Konstantin <markin_konstantin@vk.com>
2022-12-03 15:14:50 +0500

    Добавили программу которая выводит случайные элементы массива

---
+++ Csharph/les4/homework4/task3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 3: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный случайными числами.
+//  Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах). 
+//  Реализовать через функции. (* Доп сложность, “введите количество элементов массива”, 
+// “Введите минимальный порог случайных значений”, 
+// “Введите максимальный порог случайных значений”)
+
+
+
+int[] GenerateArray(int Length, int minRange, int maxRange)
+{
+
+    int[] array = new int[Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minRange, maxRange+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + " ");
+    }
+}
+
+int Promt(String message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Range = Promt("Введите длину массива ");
+int minValue = Promt("Введите минимальное значение ");
+int maxValue = Promt("Введите максимальное значение ");
+
+
+int [] arr = GenerateArray(Range, minValue, maxValue);
+PrintArray(arr);
