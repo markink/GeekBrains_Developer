ead1b2806d1fffeec3133c14a52c51ee31b4d064
Markin Konstantin <markin_konstantin@vk.com>
2022-12-03 12:50:22 +0500

    Добавили программу

---
+++ Csharph/les4/les4/task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 1: Напишите программу,
+//  которая принимает на вход число 
+//  и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Promt(String message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int value1 = Promt("Введите число: ");
+
+int Counter(int value1)
+{
+    int index = 0;
+
+    while (value1 > 0)
+    {
+        value1 = value1 / 10;
+        index += 1;
+    }
+    return index;
+}
+
+Console.WriteLine($"Количество цифр в числе {value1} равно: {Counter(value1)}");
