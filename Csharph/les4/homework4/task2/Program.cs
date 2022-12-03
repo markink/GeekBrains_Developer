3c6d53b40340d3252e84dbbe9347ead492c2ec49
Markin Konstantin <markin_konstantin@vk.com>
2022-12-03 15:05:30 +0500

    Добавили программу которая выдаёт сумму цифр

---
+++ Csharph/les4/homework4/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу,
+//  которая принимает на вход число
+//   и выдаёт сумму цифр в числе.
+//   Реализовать через функции.
+
+int Promt(String message)
+{
+    Console.WriteLine($"{message}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int inputValue = Promt("Введите число: ");
+
+int SumNumbers(int value)
+{
+    int sumValues = 0;
+    while (value >= 1) {
+        sumValues = sumValues + value%10;
+        value = value/10;
+    }  
+    return sumValues;     
+}
+Console.Write("Сумма чисел будет равна: " + SumNumbers(inputValue));
