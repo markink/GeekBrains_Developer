3265f13766e69c12aedb054dae93402a3d4cc03e
Markin Konstantin <markin_konstantin@vk.com>
2022-12-03 13:14:43 +0500

    message

---
+++ Csharph/les4/les4/task4/Program.cs
@@ -0,0 +1,32 @@
+﻿// Решение в группах задач:
+// Задача 3: Напишите программу, которая выводит 
+// массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+int[] GenerateArray(int Length, int minRange, int maxRange)
+{
+
+    int[] array = new int[Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minRange, maxRange+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + " ");
+    }
+}
+
+int [] arr = GenerateArray(8, 0, 1);
+PrintArray(arr);
+
+
+
+
+
