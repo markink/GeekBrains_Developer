def66c65fcbb78a48a6d54702b8e1c242fe4c476
Markin Konstantin <markin_konstantin@vk.com>
2022-12-03 14:27:10 +0500

    Программа которая возводит число в степень

---
+++ Csharph/les4/homework4/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1: Напишите цикл, 
+// который принимает на вход два числа (A и B)
+//  и возводит число A в натуральную степень B.
+//   Реализовать через функции.
+
+int Promt (String message) {
+    Console.WriteLine($"{message}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int inputValue1 = Promt("Введите число А: ");
+int inputValue2 = Promt("Введите число B: ");
+
+
+
+int Rate (int value1, int value2) {
+    int result = 1;
+    for (int i = 1; i <= inputValue2; i++) {
+        result = result*inputValue1;
+    }
+
+    return result;
+}
+
+Console.WriteLine(Rate(inputValue1, inputValue2));
