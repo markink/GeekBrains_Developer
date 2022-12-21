e11857835e312e88d56db6c6a42814adda8cb428
Markin Konstantin <markin_konstantin@vk.com>
2022-12-21 21:04:44 +0500

    Решение задач к уроку 8

---
+++ Csharph/les8/homework/task1/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы 
+// каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+int Prompt(string message)
+{
+    Console.Write($"{message} >");              // Вывод приглашения
+    return Convert.ToInt32(Console.ReadLine()); // ввод числа
+}
+
+int[,] FillArray(int[,] list)
+{
+    for (int i = 0; i < list.GetLength(0); i++)
+    {
+        for (int j = 0; j < list.GetLength(1); j++)
+        {
+            list[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return list;
+}
+
+void PrintArrayTwo(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] ReverseRowsArrayForBiggestValue(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int item = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = item;
+                }
+            }
+        }
+    }
+    return array;
+
+}
+
+
+
+int m = Prompt("Введите количество строк > ");
+int n = Prompt("Введите колличество столбцов > ");
+
+int[,] array = new int[m, n];
+PrintArrayTwo(FillArray(array));
+Console.WriteLine("Отсортированный массив: ");
+PrintArrayTwo(ReverseRowsArrayForBiggestValue(array));
